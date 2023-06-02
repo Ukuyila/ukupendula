4e2be9efd53a13fa1fb34152fcb830e63756d6e1
drycko <donegrafiks@gmail.com>
2023-06-02 02:54:36 +0200

    fixed login redirect to wrong path

--- ukupendula/urls.py
+++ ukupendula/urls.py
@@ -6,9 +6,9 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
-    path('auth/', include('authorisation.urls')),
-    # path('dash/', include('dashboard.urls')),
+    path('', include('authorisation.urls')),
+    # path('auth/', include('authorisation.urls')),
+    path('dash/', include('dashboard.urls')),
 ]
 
 if settings.DEBUG:
