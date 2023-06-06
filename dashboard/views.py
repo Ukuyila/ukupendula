07a818b523e7056ab9a6575382ca956d67c3559e
drycko <donegrafiks@gmail.com>
2023-06-06 11:42:55 +0200

    working on saving blog sections

--- dashboard/views.py
+++ dashboard/views.py
@@ -406,21 +406,21 @@ def save_section_head(request, blog_unique_id, section_head):
 
     blog = Blog.objects.get(uniqueId=blog_unique_id)
 
-    # if blog:
-    #     saved_sect_head = SavedBlogSectionHead.objects.create(
-    #         section_head=section_head,
-    #         blog=blog,
-    #     )
-    #     saved_sect_head.save()
+    if blog:
+        saved_sect_head = SavedBlogSectionHead.objects.create(
+            section_head=section_head,
+            blog=blog,
+        )
+        saved_sect_head.save()
 
-    #     blog_section_heads = request.session['blog-sections']
-    #     context['uniqueId'] = blog.uniqueId
-    #     # adding the sections to the context
-    #     context['saved_sect_head'] = saved_sect_head
-    #     context['blog_sections'] = blog_section_heads
-    #     return redirect('select-blog-sections', slug=blog.slug)
-    # else:
-    #     return redirect('blog-sections')
+        blog_section_heads = request.session['blog-sections']
+        context['uniqueId'] = blog.uniqueId
+        # adding the sections to the context
+        context['saved_sect_head'] = saved_sect_head
+        context['blog_sections'] = blog_section_heads
+        return redirect('select-blog-sections', slug=blog.slug)
+    else:
+        return redirect('blog-sections')
 
         
 # this generates blog from saved topic
