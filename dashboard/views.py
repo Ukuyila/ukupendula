93fb7332cef205f7452c5ac5420a5dc2d38f21ce
drycko <donegrafiks@gmail.com>
2023-06-14 15:51:10 +0200

    added social media post memory

--- dashboard/views.py
+++ dashboard/views.py
@@ -328,13 +328,13 @@ def delete_blog_topic(request, uniqueId):
         if blog.profile == request.user.profile:
             blog.delete()
             messages.info(request, "Blog deleted successfully!")
-            return redirect('incomplete-blog-memory')
+            return redirect('blog-memory' 'incomplete')
         else:
             messages.error(request, "Access denied!")
-            return redirect('incomplete-blog-memory')
+            return redirect('blog-memory' 'incomplete')
     except:
         messages.error(request, "Blog not found!")
-        return redirect('incomplete-blog-memory')
+        return redirect('blog-memory' 'incomplete')
     
 
 @login_required
@@ -345,13 +345,13 @@ def delete_blog(request, uniqueId):
             blog.deleted=True
             blog.save()
             messages.info(request, "Blog deleted successfully!")
-            return redirect('blog-memory')
+            return redirect('blog-memory' 'complete')
         else:
             messages.error(request, "Access denied!")
-            return redirect('blog-memory')
+            return redirect('blog-memory' 'complete')
     except:
         messages.error(request, "Blog not found!")
-        return redirect('blog-memory')
+        return redirect('blog-memory' 'complete')
     
 
 @login_required
@@ -367,13 +367,13 @@ def delete_saved_blog(request, uniqueId):
                     s_blog.delete()
 
             messages.info(request, "Blog deleted successfully!")
-            return redirect('saved-blog-memory')
+            return redirect('blog-memory' 'saved')
         else:
             messages.error(request, "Access denied!")
-            return redirect('saved-blog-memory')
+            return redirect('blog-memory' 'saved')
     except:
         messages.error(request, "Blog not found!")
-        return redirect('saved-blog-memory')
+        return redirect('blog-memory' 'saved')
 
 
 @login_required
