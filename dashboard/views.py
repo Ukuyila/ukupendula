0aee61e0c0d82192c8448e2d4bfae823616dfdd1
drycko <donegrafiks@gmail.com>
2023-06-21 11:37:07 +0200

    testing registration

--- dashboard/views.py
+++ dashboard/views.py
@@ -13,6 +13,7 @@ from werkzeug.urls import url_parse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.conf import settings
 
 # Other Auth imports
 from django.contrib.auth.decorators import login_required
