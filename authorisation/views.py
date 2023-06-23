8fba76682c9f1fb80b33f66ae6deed76fc53e45b
drycko <donegrafiks@gmail.com>
2023-06-23 10:20:42 +0200

    added user role in the user settings

--- authorisation/views.py
+++ authorisation/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 
-from django.conf import settings
-
 # Other imports
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
@@ -84,71 +82,6 @@ def register(request):
         user.save()
 
         time.sleep(5)
-
-        lang = settings.LANGUAGE_CODE
-        profile = Profile.objects.get(user=user)
-
-        # add user team
-        new_user_team = Team.objects.create(
-            business_name='Default',
-            is_active=True,
-            business_status=True,
-            team_principal=profile.uniqueId,
-        )
-        new_user_team.save()
-
-        profile.user_team = new_user_team.uniqueId
-        profile.save()
-        time.sleep(3)
-
-        try:
-            permission = PermissionLevel.objects.get(permission_name='Manager')
-
-            #create team manager role
-            new_role = UserRole.objects.create(
-                role_name='Team Manager',
-                abbreviation='TM',
-                permission=permission,
-                user_team=profile.user_team,
-                can_write=True,
-                can_edit=True,
-                can_delete=True,
-                can_create_team=True,
-                can_edit_team=True,
-                can_delete_team=True,
-            )
-            new_role.save()
-
-            user_settings = UserSetting.objects.create(
-                lang=lang,
-                user_role=new_role,
-                profile=profile,
-            )
-            user_settings.save()
-        except:
-            pass
-
-        # create default client
-        new_client = TeamClient.objects.create(
-            client_name='Default',
-            contact_person=user.first_name,
-            industry='General',
-            client_email=email,
-            business_address='',
-            created_by=profile.uniqueId,
-            team=profile.user_team,
-        )
-        new_client.save()
-
-        # create default category
-        new_cate = ClientCategory.objects.create(
-            category_name='General',
-            description='General category',
-            created_by=profile.uniqueId,
-            team=profile.user_team,
-            client=new_client,
-        )
-        new_cate.save()
 
         # begin email verification
         # to get the domain of the current site
