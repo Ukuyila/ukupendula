b14376ed30d75d784dc4954968469fa7a9ea7cbc
drycko <donegrafiks@gmail.com>
2023-06-27 17:01:38 +0200

    testing mailer api

--- authorisation/urls.py
+++ authorisation/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
-    path('verify-email/<token>[0-9A-Za-z_\-]+)/(?P<uniqueId>[0-9A-Za-z_\-]+)/', views.activate, name='verify-email'),
+    path('verify-email/(?P<token>[0-9A-Za-z_\-]+)/(?P<uniqueId>[0-9A-Za-z_\-]+)/', views.activate, name='verify-email'),
     path('forgot-password', views.forgot_password, name='forgot-password'),
 ]
