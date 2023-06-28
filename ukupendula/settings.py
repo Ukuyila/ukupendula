688a6c57c1e5b7bb0f5a5c9a9544999c989103ec
drycko <donegrafiks@gmail.com>
2023-06-28 11:34:13 +0200

    testing server emails from writesome

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -196,8 +196,9 @@ EMAIL_HOST = 'writesome.ai'
 EMAIL_FROM = 'no-reply@writesome.ai'
 EMAIL_HOST_USER = 'no-reply@writesome.ai'
 EMAIL_HOST_PASSWORD = 'DGA-xkx8tcj7jzb4ycf'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 PASSWORD_RESET_TIMEOUT = 14400
 
