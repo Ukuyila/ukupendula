1f67f88f9dc4f2dd4606adaf2da011df2e5bf276
drycko <donegrafiks@gmail.com>
2023-06-28 11:25:55 +0200

    Save email activate url

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -192,11 +192,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Emailing settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'donegrafiks@gmail.com'
-EMAIL_HOST_USER = 'donegrafiks@gmail.com'
-EMAIL_HOST_PASSWORD = 'ypmkiknyxguzbrod'
-EMAIL_PORT = 587
+EMAIL_HOST = 'writesome.ai'
+EMAIL_FROM = 'no-reply@writesome.ai'
+EMAIL_HOST_USER = 'no-reply@writesome.ai'
+EMAIL_HOST_PASSWORD = 'DGA-xkx8tcj7jzb4ycf'
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 PASSWORD_RESET_TIMEOUT = 14400
