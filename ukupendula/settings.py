edf7d250606dece322dcaab8b21bcc4834ef143a
drycko <donegrafiks@gmail.com>
2023-07-02 20:06:08 +0200

    Testing emailing system with SMTP

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -3,10 +3,6 @@ import os
 import sys
 import dj_database_url
 
-# for dev
-from os.path import join, dirname
-from dotenv import load_dotenv, find_dotenv
-
 from django.contrib import messages
 from django.core.management.utils import get_random_secret_key
 
@@ -29,17 +25,12 @@ MESSAGE_TAGS = {
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-if DEBUG is True:
-    ALLOWED_HOSTS = ['138.68.155.44', 'localhost', 'app.writesome.ai']
-    dotenv_file = find_dotenv(".env")
-    load_dotenv(dotenv_file)
-else:
-    ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "True") == "True"
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
 
@@ -109,22 +100,21 @@ WSGI_APPLICATION = 'ukupendula.wsgi.app'
 
 if DEVELOPMENT_MODE is True:
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pendula',
-        'USER': 'ukuser',
-        'PASSWORD': 'gbHzwLohKxjf9dLFC7QqXJBY',
-        'HOST': 'localhost',
-        'PORT': '',
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'pendula',
+            'USER': 'ukuser',
+            'PASSWORD': 'gbHzwLohKxjf9dLFC7QqXJBY',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -150,6 +140,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-gb'
 
+FLAG_GB = 'dash/images/gb_flag.jpg'
+FLAG_US = 'dash/images/us_flag.jpg'
+
 TIME_ZONE = 'Africa/Johannesburg'
 
 USE_I18N = True
@@ -164,9 +157,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 
 OPENAI_API_KEYS = os.getenv("OPENAI_API_KEY")
-MAILER_API_URL = os.getenv("MAILER_API_URL", "https://api.ukudev.co.za")
-MAILER_API_KEY = os.getenv("MAILER_API_KEY", "UKU-MakNzlhMjFlYjlmZGNlY2MzY2UxMjJhODdjN2U2")
-API_KEY_OWNER = os.getenv("API_KEY_OWNER", "TTB0002")
+
 
 DJANGORESIZED_DEFAULT_SIZE = [500, 500]
 DJANGORESIZED_DEFAULT_QUALITY = 75
@@ -190,17 +181,17 @@ MEDIA_URL = '/uploads/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Emailing settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# django SMTP mail
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
-EMAIL_HOST = 'pendula.ukudev.co.za'
-EMAIL_FROM = 'hello@pendula.ukudev.co.za'
-EMAIL_HOST_USER = 'hello@pendula.ukudev.co.za'
-EMAIL_HOST_PASSWORD = 'DGA-xkx8tcj7jzb4ycf'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv("EMAIL_HOST", "branding.ukupendula.ai")
+EMAIL_PORT = os.getenv("EMAIL_PORT", "587")
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "no-reply@ukupendula.ai")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "no-reply@ukupendula.ai")
 
 PASSWORD_RESET_TIMEOUT = 14400
 
@@ -209,6 +200,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 #payfast.io
-PAYFAST_MERCHANT_ID = '10024789'
-PAYFAST_MERCHANT_KEY = 'dtz5khr0cbz74'
+PAYFAST_MERCHANT_ID = os.getenv("PAYFAST_MERCHANT_ID")
+PAYFAST_MERCHANT_KEY = os.getenv("PAYFAST_MERCHANT_KEY")
 PAYFAST_URL_BASE = ''
