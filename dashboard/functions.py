ed2dfef090b752bdd27b515f403129059f04143c
drycko <donegrafiks@gmail.com>
2023-07-04 13:43:32 +0200

    test registrations

--- dashboard/functions.py
+++ dashboard/functions.py
@@ -804,19 +804,19 @@ def check_user_lang(profile, lang):
     return lang
 
 
-def populate_defaults():
-    # populate default ToneOfVoice
-    tones = [
-        'Funny', 'Casual', 'Excited', 'Professional', 'Witty', 'Friendly', 'Sarcastic',
-        'Expressive', 'Direct', 'Playful', 'Feminine', 'Masculine', 'Bold', 'Uplifting',
-        'Dramatic', 'Grumpy', 'Motivating', 'Secretive', 'Sophisticated','Positive',
-        'Confident', 'Educational'
-    ]
+# def populate_defaults():
+#     # populate default ToneOfVoice
+#     tones = [
+#         'Funny', 'Casual', 'Excited', 'Professional', 'Witty', 'Friendly', 'Sarcastic',
+#         'Expressive', 'Direct', 'Playful', 'Feminine', 'Masculine', 'Bold', 'Uplifting',
+#         'Dramatic', 'Grumpy', 'Motivating', 'Secretive', 'Sophisticated','Positive',
+#         'Confident', 'Educational'
+#     ]
 
-    for tone in tones:
-        try:
-            tone_exist = ToneOfVoice.objects.get(tone_of_voice=tone)
-        except:
-            ToneOfVoice.objects.create(tone_of_voice=tone)
+#     for tone in tones:
+#         try:
+#             tone_exist = ToneOfVoice.objects.get(tone_of_voice=tone)
+#         except:
+#             ToneOfVoice.objects.create(tone_of_voice=tone)
 
 #
