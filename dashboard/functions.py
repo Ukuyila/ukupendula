57539bfe80481d9f59473f51fafe5f0e3e942c84
drycko <donegrafiks@gmail.com>
2023-08-10 00:04:54 +0200

    Add notifications database and reset monthly numbers on profile

--- dashboard/functions.py
+++ dashboard/functions.py
@@ -766,9 +766,9 @@ def round_to_multiple(number, multiple, direction='nearest'):
 
 def user_notices(profile):
     notifications = []
-    # user_notices = UserNotification.objects.filter(profile=profile).order_by('date_created')
-    # for notice in user_notices:
-    #     notifications.append(notice)
+    user_notices = UserNotification.objects.filter(profile=profile).order_by('date_created')
+    for notice in user_notices:
+        notifications.append(notice)
     return notifications
 
 
