28de95a6d860edf1eb5b46bbe5ca81f448f40763
drycko <donegrafiks@gmail.com>
2023-08-14 00:34:10 +0200

    conditional static root url

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -210,7 +210,7 @@ AWS_ACCESS_KEY_ID = 'DO00H2CVHFYX93WKX73R'
 AWS_SECRET_ACCESS_KEY = 'BwyF/bqMqEhjrY99JUerBy0vY9hiLQUKowfuY14V7/8'
 
 AWS_STORAGE_BUCKET_NAME = 'writesome'
-AWS_S3_ENDPOINT_URL = 'http://writesome.syd1.digitaloceanspaces.com'
+AWS_S3_ENDPOINT_URL = 'https://writesome.syd1.digitaloceanspaces.com'
 AWS_S3_CUSTOM_DOMAIN = 'writesome.syd1.cdn.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
@@ -220,7 +220,7 @@ AWS_DEFAULT_ACL = 'public-read'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATIC_ROOT = 'static/'
 
 # Default primary key field type
