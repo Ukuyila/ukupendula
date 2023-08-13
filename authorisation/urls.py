c269993c0b2f6974ca8009c53753d30be4d5cc43
drycko <donegrafiks@gmail.com>
2023-08-13 20:46:56 +0200

    conditional static root url

--- authorisation/urls.py
+++ authorisation/urls.py
@@ -12,9 +12,9 @@ from django.contrib.auth.views import (
 
 urlpatterns = [
     path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('logout', views.logout, name='logout'),
-    path('activate/<uidb64>/<token>', views.activate, name='activate'),
+    # path('register', views.register, name='register'),
+    # path('logout', views.logout, name='logout'),
+    # path('activate/<uidb64>/<token>', views.activate, name='activate'),
     # path('forgot-password', views.forgot_password, name='forgot-password'),
 
     path('password-reset/', PasswordResetView.as_view(template_name='authorisation/forgot-password.html'),name='password-reset'),
