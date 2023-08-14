f08e29ddc81019d9658fb18e0c4091df64c321f6
drycko <donegrafiks@gmail.com>
2023-08-14 16:56:38 +0200

    secure deployment with nginx

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -215,7 +215,7 @@ else:
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
-    AWS_LOCATION = 'writesome/static'
+    AWS_LOCATION = 'static'
     AWS_DEFAULT_ACL = 'public-read'
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
