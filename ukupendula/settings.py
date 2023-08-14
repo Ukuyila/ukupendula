f9f0edca32fee46c76e94120fca614efd7e1ddae
drycko <donegrafiks@gmail.com>
2023-08-14 20:14:49 +0200

    static files not collecting

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -216,7 +216,7 @@ else:
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
-    AWS_LOCATION = 'static'
+    AWS_LOCATION = 'writesome/static'
     AWS_DEFAULT_ACL = 'public-read'
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
