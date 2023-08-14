62ce839e2dfa157836e6a35ee9e1b44e63a26d59
drycko <donegrafiks@gmail.com>
2023-08-14 16:00:11 +0200

    secure deployment with nginx

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -211,7 +211,7 @@ else:
 
     AWS_STORAGE_BUCKET_NAME = 'writesome'
     AWS_S3_ENDPOINT_URL = 'https://writesome.syd1.digitaloceanspaces.com'
-    AWS_S3_CUSTOM_DOMAIN = 'writesome.syd1.cdn.digitaloceanspaces.com'
+    # AWS_S3_CUSTOM_DOMAIN = 'writesome.syd1.cdn.digitaloceanspaces.com'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
@@ -220,7 +220,7 @@ else:
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-    STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
     STATIC_ROOT = 'static/'
 
 # Default primary key field type
