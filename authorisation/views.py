c18b6dd8065f0cd02ebf7a5e834d82bdc32f274c
drycko <donegrafiks@gmail.com>
2023-08-16 14:23:01 +0200

    testing zoho mailing

--- authorisation/views.py
+++ authorisation/views.py
@@ -112,7 +112,8 @@ def zohoEmailVerification(request, user, password1, user_team):
 <html>
 <head></head>
 <body>
-    <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Verify Your Email Address</h1>
+<h5 style="margin: 0; font-size: 26px; font-weight: 500; letter-spacing: -1px; line-height: 48px;">Hi, { user.username}</h5>
+    <h5 style="margin: 0; font-size: 26px; font-weight: 500; letter-spacing: -1px; line-height: 48px;">Verify Your Email Address</h5>
     <p style="margin: 0;">Tap the button below to confirm your email address. If you didn't create an account with <a href=" { curr_domain }">writesome.ai</a>, you can safely delete this email.</p>
     <div align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
     <a href="{ 'https' if request.is_secure() else 'http' }://{ get_current_site(request).domain }/activate/{ urlsafe_base64_encode(force_bytes(user.pk)) }/{ account_activation_token.make_token(user) }" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
