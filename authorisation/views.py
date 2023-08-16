d46537d35e89daf2edd6267711db1777bc7ca073
drycko <donegrafiks@gmail.com>
2023-08-16 18:17:03 +0200

    testing zoho mailing

--- authorisation/views.py
+++ authorisation/views.py
@@ -123,6 +123,78 @@ def zohoEmailVerification(request, user, password1, user_team):
 </body>
 </html>
 '''
+    
+    message = f'''\
+<html>
+<head></head>
+<body>
+<table cellspacing="0" cellpadding="0" style="margin:0px auto; width: 100%; background-color:#fff;">
+    <tbody>
+        <tr>
+            <td>
+                <div
+                    style="background-color: #fff; border: 1px solid #eee; box-sizing: border-box; font-family: Lato, Helvetica, 'Helvetica Neue', Arial, 'sans-serif'; margin: auto; max-width: 600px; overflow: hidden; width: 600px;">
+                    <div
+                        style="padding: 65px 90px 20px; background-color: #1B3E71; background-image: url(https://static.zohocdn.com/zeptomail/assets/images/circles.4ee9fbd3db3cd183c76b.svg); background-repeat: no-repeat; background-position: top right; background-size: 140px;">
+                        <h4 style="color: #fff; font-weight: normal; font-size: 16px; margin: 0; margin-bottom: 10px;">
+                            Hi { user.username },<br></h4>
+                        <h2 style="color: #fff; font-size: 24px; font-weight: normal;margin: 0;">Welcome to
+                            writesome!<br></h2>
+                    </div>
+                    <div style="padding: 25px 90px 65px;">
+                        <p style="margin: 0px; line-height: 20px;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">We're very glad that you have chosen <b>writesome</b> for your business.</span><br>
+                        </p>
+                        <p style="margin: 25px 0px 0px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 25px 0px 0px; line-height: 20px;">Your login
+                            information:</span><br></p>
+                        <div style="font-size: 14px; margin: 20px 0 20px 20px;"><br></div>
+                        <div
+                            style="background-color: #F5F7FA; border-radius: 8px; font-size: 14px; padding: 20px; margin-bottom: 30px;">
+                            <div style="display: flex; margin-bottom: 15px;">
+                                <label style="color: #647282; width: 70px;">Login URL</label>
+                                <span class="colour" style="color: rgb(100, 114, 130); margin-right: 10px;">:</span> <a
+                                    style="color: #1B3E71; text-decoration: none;"
+                                    href="{  'https' if request.is_secure() else 'http' }://{ get_current_site(request).domain }/auth/login">Login</a><br></div>
+                            <div style="display: flex;"><label style="color: #647282; width: 70px;">Username</label>
+                                <span class="colour" style="color: rgb(100, 114, 130); margin-right: 10px;">:</span>
+                                <div>{ user.username }<br></div>
+                            </div>
+                        </div>
+                        <p style="margin: 0px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">To get started with using your
+                            account, verify your account by clicking on the below link:</span><br></p><a
+                            href="{ 'https' if request.is_secure() else 'http' }://{ get_current_site(request).domain }/auth/activate/{ urlsafe_base64_encode(force_bytes(user.pk)) }/{ account_activation_token.make_token(user) }"
+                            style="border: none; border-radius: 4px; color: #fff; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; text-decoration: none; padding: 12px 24px; background-color: #1B3E71; margin: 20px 0 30px;">Verify
+                            your account</a>
+                        <p style="margin: 0px 0px 30px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px 0px 30px; line-height: 20px;">If you'd like to
+                            know more about writesome or want to get in touch with us, get in touch with our
+                            customer support team.</span><br></p>
+                        <p style="margin: 0px 0px 30px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px 0px 30px; line-height: 20px;">If you're looking
+                            for immediate help, take a look at our help documentation and view our latest updates in our
+                            blog.</span><br></p>
+                        <p style="margin: 0px; line-height: 20px;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">Thank you,</span><br>
+                        </p>
+                        <p style="margin: 0px; line-height: 20px;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">Team { settings.APP_NAME }.</span><br>
+                        </p>
+                        <div><br></div>
+                        <div class="align-center" style="text-align: center;"><br></div>
+                        <div class="align-center" style="text-align: center;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;"><img src="portal/v1.0/mailagents/2d6f.b034a70cd9499d2.2cf853c932fd4979/files/2d6f.b034a70cd9499d2.h1.bfc16120-3c17-11ee-a5ac-525400d6cd4f.189fdb00832/download" orig_width="400" orig_height="101" width="160" height="40" size_option="small"></span><br>
+                        </div>
+                        <div class="align-center" style="text-align: center;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">A product by <a href="https://ukuyila.com" target="_blank">Ukuyila (Pty)Ltd</a></span><br>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<div><br></div>
+</body>
+</html>
+    '''
 
     port = 465
     smtp_server = settings.EMAIL_HOST
@@ -133,7 +205,7 @@ def zohoEmailVerification(request, user, password1, user_team):
     msg['From'] = formataddr(("writesome", "noreply@writesome.ai"))
     msg['Reply-To'] = settings.EMAIL_REPLY_TO
     msg['To'] = user.email
-    msg.set_content(html_message, subtype='html')
+    msg.set_content(message, subtype='html')
     # msg.add_alternative(html_message, subtype='html')
 
     try:
