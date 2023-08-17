42641980c37821e1df68a63ca4aad4a6701d1bb0
drycko <donegrafiks@gmail.com>
2023-08-17 16:15:55 +0200

    read notifications on click

--- authorisation/views.py
+++ authorisation/views.py
@@ -88,23 +88,7 @@ def login(request):
     return render(request, 'authorisation/login.html', context)
 
 
-def zohoEmailVerification(request, user, password1, user_team):
-    resp_msg = ''
-    mail_subject = "Verify your email address"
-    html_message = render_to_string("authorisation/email-verification.html", {
-        'user': user.username,
-        'domain': get_current_site(request).domain,
-        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-        'token': account_activation_token.make_token(user),
-        'protocol': 'https' if request.is_secure() else 'http',
-        'password': password1,
-        'user_team': user_team.business_name,
-        'email': user.email,
-        'reply_to': settings.EMAIL_REPLY_TO,
-        'type_of_action': 'email verification',
-    })
-    
-    # headers = {"Message-ID": str(uuid4())}
+def emailVerificationApi(request, user, password1, user_team):
 
     url = "https://api.writesome.ai/mailer-api/welcome-email.php"
 
@@ -125,119 +109,139 @@ def zohoEmailVerification(request, user, password1, user_team):
     'content-type': "application/json",
     'authorization': "Zoho-enczapikey wSsVR60nqxHzC6Yozj2udLo8nglQU1vwFRl+2geguiP5T/zK9sc/k0HIVw/zGqAcGDQ6RjJGpO4oyx4F1jpb3Ikqy1lVASiF9mqRe1U4J3x17qnvhDzIXGlckxSKLIwLww1tmGVpE89u",
     }
-    
-    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
-    return json.load(response.text)
-    # print(response.text)
+
+    try:
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+        return json.load(response.text.decode('utf-8'))
+    except:
+        return f'Problem sending email to {user.email}, check if you typed it correctly.'
+
+
+def zohoEmailVerification(request, user, password1, user_team):
+    resp_msg = ''
+    mail_subject = "Verify your email address"
+    html_message = render_to_string("authorisation/email-verification.html", {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http',
+        'password': password1,
+        'user_team': user_team.business_name,
+        'email': user.email,
+        'reply_to': settings.EMAIL_REPLY_TO,
+        'type_of_action': 'email verification',
+    })
+
     # message_cid = make_msgid()
-    
+    headers = {"Message-ID": str(uuid4())}
     # email = EmailMessage(mail_subject, message, to=[user.email], reply_to=[settings.EMAIL_REPLY_TO], headers=headers)
     # email.content_subtype = 'html'
-#     curr_domain = get_current_site(request).domain
-#     message = f'''\
-# <html>
-# <head></head>
-# <body>
-# <table cellspacing="0" cellpadding="0" style="margin:0px auto; width: 100%; background-color:#fff;">
-#     <tbody>
-#         <tr>
-#             <td>
-#                 <div
-#                     style="background-color: #fff; border: 1px solid #eee; box-sizing: border-box; font-family: Lato, Helvetica, 'Helvetica Neue', Arial, 'sans-serif'; margin: auto; max-width: 600px; overflow: hidden; width: 600px;">
-#                     <div
-#                         style="padding: 65px 90px 20px; background-color: #1B3E71; background-image: url(https://static.zohocdn.com/zeptomail/assets/images/circles.4ee9fbd3db3cd183c76b.svg); background-repeat: no-repeat; background-position: top right; background-size: 140px;">
-#                         <h4 style="color: #fff; font-weight: normal; font-size: 16px; margin: 0; margin-bottom: 10px;">
-#                             Hi { user.username },<br></h4>
-#                         <h2 style="color: #fff; font-size: 24px; font-weight: normal;margin: 0;">Welcome to
-#                             writesome!<br></h2>
-#                     </div>
-#                     <div style="padding: 25px 90px 65px;">
-#                         <p style="margin: 0px; line-height: 20px;">
-#                             <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">We're very glad that you have chosen <b>writesome</b> for your business.</span><br>
-#                         </p>
-#                         <p style="margin: 25px 0px 0px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 25px 0px 0px; line-height: 20px;">Your login
-#                             information:</span><br></p>
-#                         <div style="font-size: 14px; margin: 20px 0 20px 20px;"><br></div>
-#                         <div
-#                             style="background-color: #F5F7FA; border-radius: 8px; font-size: 14px; padding: 20px; margin-bottom: 30px;">
-#                             <div style="display: flex; margin-bottom: 15px;">
-#                                 <label style="color: #647282; width: 70px;">Login URL</label>
-#                                 <span class="colour" style="color: rgb(100, 114, 130); margin-right: 10px;">:</span> <a
-#                                     style="color: #1B3E71; text-decoration: none;"
-#                                     href="{  'https' if request.is_secure() else 'http' }://{ get_current_site(request).domain }/auth/login">Login</a><br></div>
-#                             <div style="display: flex;"><label style="color: #647282; width: 70px;">Username</label>
-#                                 <span class="colour" style="color: rgb(100, 114, 130); margin-right: 10px;">:</span>
-#                                 <div>{ user.username }<br></div>
-#                             </div>
-#                         </div>
-#                         <p style="margin: 0px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">To get started with using your
-#                             account, verify your account by clicking on the below link:</span><br></p><a
-#                             href="{ 'https' if request.is_secure() else 'http' }://{ get_current_site(request).domain }/auth/activate/{ urlsafe_base64_encode(force_bytes(user.pk)) }/{ account_activation_token.make_token(user) }"
-#                             style="border: none; border-radius: 4px; color: #fff; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; text-decoration: none; padding: 12px 24px; background-color: #1B3E71; margin: 20px 0 30px;">Verify
-#                             your account</a>
-#                         <p style="margin: 0px 0px 30px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px 0px 30px; line-height: 20px;">If you'd like to
-#                             know more about writesome or want to get in touch with us, get in touch with our
-#                             customer support team.</span><br></p>
-#                         <p style="margin: 0px 0px 30px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px 0px 30px; line-height: 20px;">If you're looking
-#                             for immediate help, take a look at our help documentation and view our latest updates in our
-#                             blog.</span><br></p>
-#                         <p style="margin: 0px; line-height: 20px;">
-#                             <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">Thank you,</span><br>
-#                         </p>
-#                         <p style="margin: 0px; line-height: 20px;">
-#                             <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">Team.</span><br>
-#                         </p>
-#                         <div><br></div>
-#                         <div class="align-center" style="text-align: center;"><br></div>
-#                         <div class="align-center" style="text-align: center;">
-#                             <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">{ settings.APP_NAME }</span><br>
-#                         </div>
-#                         <div class="align-center" style="text-align: center;">
-#                             <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">A product by <a  target="_blank">Ukuyila (Pty)Ltd</a></span><br>
-#                         </div>
-#                     </div>
-#                 </div>
-#             </td>
-#         </tr>
-#     </tbody>
-# </table>
-# <div><br></div>
-# </body>
-# </html>
-#     '''
+    curr_domain = get_current_site(request).domain
+    message = f'''\
+<html>
+<head></head>
+<body>
+<table cellspacing="0" cellpadding="0" style="margin:0px auto; width: 100%; background-color:#fff;">
+    <tbody>
+        <tr>
+            <td>
+                <div
+                    style="background-color: #fff; border: 1px solid #eee; box-sizing: border-box; font-family: Lato, Helvetica, 'Helvetica Neue', Arial, 'sans-serif'; margin: auto; max-width: 600px; overflow: hidden; width: 600px;">
+                    <div
+                        style="padding: 65px 90px 20px; background-color: #1B3E71; background-image: url(https://static.zohocdn.com/zeptomail/assets/images/circles.4ee9fbd3db3cd183c76b.svg); background-repeat: no-repeat; background-position: top right; background-size: 140px;">
+                        <h4 style="color: #fff; font-weight: normal; font-size: 16px; margin: 0; margin-bottom: 10px;">
+                            Hi { user.username },<br></h4>
+                        <h2 style="color: #fff; font-size: 24px; font-weight: normal;margin: 0;">Welcome to
+                            writesome!<br></h2>
+                    </div>
+                    <div style="padding: 25px 90px 65px;">
+                        <p style="margin: 0px; line-height: 20px;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">We're very glad that you have chosen <b>writesome</b> for your business.</span><br>
+                        </p>
+                        <p style="margin: 25px 0px 0px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 25px 0px 0px; line-height: 20px;">Your login
+                            information:</span><br></p>
+                        <div style="font-size: 14px; margin: 20px 0 20px 20px;"><br></div>
+                        <div
+                            style="background-color: #F5F7FA; border-radius: 8px; font-size: 14px; padding: 20px; margin-bottom: 30px;">
+                            <div style="display: flex; margin-bottom: 15px;">
+                                <label style="color: #647282; width: 70px;">Login URL</label>
+                                <span class="colour" style="color: rgb(100, 114, 130); margin-right: 10px;">:</span> <a
+                                    style="color: #1B3E71; text-decoration: none;"
+                                    href="{  'https' if request.is_secure() else 'http' }://{ get_current_site(request).domain }/auth/login">Login</a><br></div>
+                            <div style="display: flex;"><label style="color: #647282; width: 70px;">Username</label>
+                                <span class="colour" style="color: rgb(100, 114, 130); margin-right: 10px;">:</span>
+                                <div>{ user.username }<br></div>
+                            </div>
+                        </div>
+                        <p style="margin: 0px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">To get started with using your
+                            account, verify your account by clicking on the below link:</span><br></p><a
+                            href="{ 'https' if request.is_secure() else 'http' }://{ get_current_site(request).domain }/auth/activate/{ urlsafe_base64_encode(force_bytes(user.pk)) }/{ account_activation_token.make_token(user) }"
+                            style="border: none; border-radius: 4px; color: #fff; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; text-decoration: none; padding: 12px 24px; background-color: #1B3E71; margin: 20px 0 30px;">Verify
+                            your account</a>
+                        <p style="margin: 0px 0px 30px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px 0px 30px; line-height: 20px;">If you'd like to
+                            know more about writesome or want to get in touch with us, get in touch with our
+                            customer support team.</span><br></p>
+                        <p style="margin: 0px 0px 30px; line-height: 20px;"><span class="size" style="font-size: 14px; margin: 0px 0px 30px; line-height: 20px;">If you're looking
+                            for immediate help, take a look at our help documentation and view our latest updates in our
+                            blog.</span><br></p>
+                        <p style="margin: 0px; line-height: 20px;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">Thank you,</span><br>
+                        </p>
+                        <p style="margin: 0px; line-height: 20px;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">Team.</span><br>
+                        </p>
+                        <div><br></div>
+                        <div class="align-center" style="text-align: center;"><br></div>
+                        <div class="align-center" style="text-align: center;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">{ settings.APP_NAME }</span><br>
+                        </div>
+                        <div class="align-center" style="text-align: center;">
+                            <span class="size" style="font-size: 14px; margin: 0px; line-height: 20px;">A product by <a  target="_blank">Ukuyila (Pty)Ltd</a></span><br>
+                        </div>
+                    </div>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<div><br></div>
+</body>
+</html>
+    '''
 
-#     port = 465
-#     smtp_server = settings.EMAIL_HOST
-#     username="emailapikey"
-#     password = settings.EMAIL_HOST_PASSWORD
-#     msg = EmailMessage()
-#     msg['Subject'] = mail_subject
-#     msg['From'] = formataddr(("writesome", "noreply@writesome.ai"))
-#     msg['Reply-To'] = settings.EMAIL_REPLY_TO
-#     msg['To'] = user.email
-#     msg.set_content(message, subtype='html')
-#     # msg.add_alternative(html_message, subtype='html')
+    port = 465
+    smtp_server = settings.EMAIL_HOST
+    username="emailapikey"
+    password = settings.EMAIL_HOST_PASSWORD
+    msg = EmailMessage()
+    msg['Subject'] = mail_subject
+    msg['From'] = formataddr(("writesome", "noreply@writesome.ai"))
+    msg['Reply-To'] = settings.EMAIL_REPLY_TO
+    msg['To'] = user.email
+    msg.set_content(message, subtype='html')
+    # msg.add_alternative(html_message, subtype='html')
 
-#     try:
-#         if port == 465:
-#             context = ssl.create_default_context()
-#             with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#                 server.login(username, password)
-#                 server.send_message(msg)
-#         elif port == 587:
-#             with smtplib.SMTP(smtp_server, port) as server:
-#                 server.starttls()
-#                 server.login("emailapikey", password)
-#                 server.send_message(msg)
-#         else:
-#             resp_msg = f'use 465 / 587 as port value.'
-#             # exit()
-#         resp_msg = f'Account successfully created, please go to your email {user.email} inbox and click on \
-#             received activation link to confirm and complete the registration. Note: If not found check spam folder.'
-#     except Exception as e:
-#         # context['test_email'] = f'error: {e}'
-#         resp_msg = f'Problem sending email to {user.email}, check if you typed it correctly. error: {e}'
-#     return resp_msg
+    try:
+        if port == 465:
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(username, password)
+                server.send_message(msg)
+        elif port == 587:
+            with smtplib.SMTP(smtp_server, port) as server:
+                server.starttls()
+                server.login("emailapikey", password)
+                server.send_message(msg)
+        else:
+            resp_msg = f'use 465 / 587 as port value.'
+            # exit()
+        resp_msg = f'Account successfully created, please go to your email {user.email} inbox and click on \
+            received activation link to confirm and complete the registration. Note: If not found check spam folder.'
+    except Exception as e:
+        # context['test_email'] = f'error: {e}'
+        resp_msg = f'Problem sending email to {user.email}, check if you typed it correctly. error: {e}'
+    return resp_msg
 
 
 def emailVerification(request, user, password1, user_team):
@@ -369,7 +373,7 @@ def register(request):
         new_cate.save()
         
         # begin email verification
-        email = zohoEmailVerification(request, user, password1, new_user_team)
+        email = emailVerificationApi(request, user, password1, new_user_team)
 
         messages.info(request, email)
         return redirect('login')
