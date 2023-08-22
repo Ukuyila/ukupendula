334f85b97d964d2f3af1dd4e5cf94832f238f6c0
drycko <donegrafiks@gmail.com>
2023-08-22 13:09:42 +0200

    fixing static files location

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -199,14 +199,14 @@ DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {'PNG': ".png"}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-USE_SPACES = False
+USE_SPACES = True
 if USE_SPACES:
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
-    AWS_S3_ENDPOINT_URL = os.getenv("AWS_ENDPOINT_URL")
-    # AWS_S3_CUSTOM_DOMAIN = os.getenv("AWS_CUSTOM_DOMAIN")
+    AWS_STORAGE_BUCKET_NAME = 'writesome'
+    AWS_S3_ENDPOINT_URL = 'https://writesome.syd1.digitaloceanspaces.com'
+    AWS_S3_CUSTOM_DOMAIN = 'writesome.syd1.cdn.digitaloceanspaces.com'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
@@ -216,10 +216,10 @@ if USE_SPACES:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
-    STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+    STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATIC_ROOT = 'staticfiles/'
 
-    MEDIA_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, 'uploads')
+    MEDIA_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, 'uploads')
     MEDIA_ROOT = '/uploads/'
 else:
     STATIC_URL = '/static/'
