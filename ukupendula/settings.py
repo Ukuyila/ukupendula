3e3613c78c109adfa583a0416f68a2abfa44776c
drycko <donegrafiks@gmail.com>
2023-08-22 14:29:34 +0200

    fixing static files location

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -210,13 +210,13 @@ if USE_SPACES:
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
-    AWS_LOCATION = 'static'
+    AWS_LOCATION = 'writesome/static'
     AWS_DEFAULT_ACL = 'public-read'
 
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
-    STATIC_URL = '{}/writesome/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATIC_ROOT = 'static/'
 
     MEDIA_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, 'uploads')
