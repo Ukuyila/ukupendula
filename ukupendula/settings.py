d5aaaf40efe70f8a07af85817d00700f03b0d38d
drycko <donegrafiks@gmail.com>
2023-08-22 12:07:19 +0200

    fixing static files location

--- ukupendula/settings.py
+++ ukupendula/settings.py
@@ -223,7 +223,7 @@ if USE_SPACES:
     MEDIA_ROOT = '/uploads/'
 else:
     STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
     MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
     MEDIA_URL = '/uploads/'
