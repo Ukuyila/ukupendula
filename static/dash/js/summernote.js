b0959a79da420fd6cd9f790dd35205c67796cce1
drycko <donegrafiks@gmail.com>
2023-08-23 09:46:42 +0200

    added SEO meta to the app

--- static/dash/js/summernote.js
+++ static/dash/js/summernote.js
@@ -1,6 +1,17 @@
 jQuery(function(e) {
     'use strict';
     $(document).ready(function() {
-        $('#summernote').summernote();
+        $('#summernote').summernote({
+            toolbar: [
+              // [groupName, [list of button]]
+            //   ['style', ['bold', 'italic', 'underline', 'clear']],
+            //   ['font', ['strikethrough', 'superscript', 'subscript']],
+            //   ['fontsize', ['fontsize']],
+            //   ['color', ['color']],
+            //   ["view", ["fullscreen", "codeview", "help"]]
+            //   ['para', ['ul', 'ol', 'paragraph']],
+            //   ['height', ['height']]
+            ]
+        });
     });
 });
